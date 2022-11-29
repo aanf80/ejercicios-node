32cb716732066040c45d0dcb7de03981088b1243
aanf80 <armando.navarroflores94@gmail.com>
2022-11-28 18:11:09 -0700

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var rect = {
+/*var rect = {
     perimeter: (x,y) => (2*(x+y)),
     area: (x,y) => (x*y)
-};
+};*/
+
+var rect = require('./rectangle');
 
 function solveRect(l,b){
-    console.log("Solving for rectangle with l = "+l+" and b = "+b);
+    console.log("Solving for rectangle v2 with l = "+l+" and b = "+b);
 
     if(l <= 0 || b <= 0){
         console.log("Rectangle dimension should be greater than zero");
